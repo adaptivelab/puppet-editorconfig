4b62d2be178f8fcd4b36b4ec3b62a0e6268a61d8
Chris Reeves <hello@chris.reeves.io>
2013-08-23 10:42:20 +0100

    Fixing syntax?

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@ class editorconfig {
 
   file { "/Users/${::luser}/.editorconfig":
     mode => 0644,
-    owner => ${::luser},
-    group => ${::luser},
+    owner => "${::luser}",
+    group => "${::luser}",
     source => 'puppet:///modules/editorconfig/files/.editorconfig'
   }
 
